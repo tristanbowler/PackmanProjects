b7f26aa33990349bd49dd571333bbd34adf88c03
Tristan Bowler <tristan.bowler@gmail.com>
2020-04-12 00:00:01 -0600

    Through q5

--- Project_3_Bowler/p03/reinforcement/analysis.py
+++ Project_3_Bowler/p03/reinforcement/analysis.py
@@ -21,41 +21,46 @@
 
 def question2():
     answerDiscount = 0.9
-    answerNoise = 0.2
+    answerNoise = 0.01
     return answerDiscount, answerNoise
 
 def question3a():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    "Prefer the close exit (+1), risking the cliff (-10)"
+    answerDiscount = 0.25
+    answerNoise = 0.0
+    answerLivingReward = 0.2
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3b():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    "Prefer the close exit (+1), but avoiding the cliff (-10)"
+    answerDiscount = 0.25
+    answerNoise = 0.1
+    answerLivingReward = 0.2
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3c():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    "Prefer the distant exit (+10), risking the cliff (-10)"
+    answerDiscount = 0.25
+    answerNoise = 0.0
+    answerLivingReward = 2.0
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3d():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    "Prefer the distant exit (+10), avoiding the cliff (-10)"
+    answerDiscount = 0.25
+    answerNoise = 0.1
+    answerLivingReward = 2.0
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3e():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    "Avoid both exits and the cliff (so an episode should never terminate)"
+    answerDiscount = 0.25
+    answerNoise = 0.1
+    answerLivingReward = 20.0
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
--- Project_3_Bowler/p03/reinforcement/valueIterationAgents.py
+++ Project_3_Bowler/p03/reinforcement/valueIterationAgents.py
@@ -61,7 +61,28 @@ class ValueIterationAgent(ValueEstimationAgent):
 
     def runValueIteration(self):
         # Write value iteration code here
-        "*** YOUR CODE HERE ***"
+        """*** YOUR CODE HERE ***"""
+        for i in range(0, self.iterations):
+            # Create a new counter
+            state_counter = util.Counter()
+            states = self.mdp.getStates()
+
+            for state in states:
+                if not self.mdp.isTerminal(state):  # This just saves a little bit of memory
+                    # Create another new counter
+                    actions_counter = util.Counter()
+                    actions = self.mdp.getPossibleActions(state)
+
+                    for action in actions:
+                        # Set Q Value for action
+                        actions_counter[action] = self.computeQValueFromValues(state, action)
+
+                    # argMax returns the key with the biggest value in the counter
+                    max_action = actions_counter.argMax()
+                    state_counter[state] = actions_counter[max_action]  # Max Q Value
+
+            # Update the values list before the next iteration
+            self.values = state_counter
 
 
     def getValue(self, state):
@@ -77,7 +98,26 @@ class ValueIterationAgent(ValueEstimationAgent):
           value function stored in self.values.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        q = 0.0
+
+        # Transition states and probs return type:
+        "Returns list of(nextState, prob) pairs representing the states reachable from " \
+            " 'state' by taking 'action' along with their transition probabilities."
+        transition_states = self.mdp.getTransitionStatesAndProbs(state, action)  # list(tuple(nextState, prob))
+
+        for next_state, probability in transition_states:
+
+            # Apply discount
+            discounted_value = self.discount * self.values[next_state]
+
+            # Get reward
+            "Get reward for state, action, nextState transition. Note that the reward depends only on the state " \
+                "being departed (as in the R+N book examples, which more or less use this convention)."
+            reward = self.mdp.getReward(state, action, next_state)  # float
+
+            q += probability * (discounted_value + reward)
+
+        return q
 
     def computeActionFromValues(self, state):
         """
@@ -89,7 +129,16 @@ class ValueIterationAgent(ValueEstimationAgent):
           terminal state, you should return None.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        if self.mdp.isTerminal(state):
+            return None
+
+        actions_counter = util.Counter()
+        actions = self.mdp.getPossibleActions(state)
+
+        for action in actions:
+            actions_counter[action] = self.computeQValueFromValues(state, action)
+
+        return actions_counter.argMax()
 
     def getPolicy(self, state):
         return self.computeActionFromValues(state)
@@ -131,6 +180,29 @@ class AsynchronousValueIterationAgent(ValueIterationAgent):
     def runValueIteration(self):
         "*** YOUR CODE HERE ***"
 
+        states = self.mdp.getStates()
+
+        for i in range(0, self.iterations):
+            # this is the key. It makes the algorithm rotate through the states for the number of iterations
+            # instead of doing all of the states * the number of iterations
+            state = states[i % len(states)]
+            if not self.mdp.isTerminal(state):
+                # Same as above
+                if not self.mdp.isTerminal(state):  # This just saves a little bit of memory
+                    # Create another new counter
+                    actions_counter = util.Counter()
+                    actions = self.mdp.getPossibleActions(state)
+
+                    for action in actions:
+                        # Set Q Value for action
+                        actions_counter[action] = self.computeQValueFromValues(state, action)
+
+                    # argMax returns the key with the biggest value in the counter
+                    max_action = actions_counter.argMax()
+                    self.values[state] = actions_counter[max_action]
+
+
+
 class PrioritizedSweepingValueIterationAgent(AsynchronousValueIterationAgent):
     """
         * Please read learningAgents.py before reading this.*
@@ -150,4 +222,63 @@ class PrioritizedSweepingValueIterationAgent(AsynchronousValueIterationAgent):
 
     def runValueIteration(self):
         "*** YOUR CODE HERE ***"
+        queue = util.PriorityQueue()
+        state_counter = util.Counter()
+        states = self.mdp.getStates()
 
+        predecessors = {}  # dict of <state, list([state])>
+
+        for state in states:
+            if not self.mdp.isTerminal(state):
+                actions_counter = util.Counter()
+                actions = self.mdp.getPossibleActions(state)
+
+                # This sets up the dict of predecessors for each action state
+                for action in actions:
+                    transition_states = self.mdp.getTransitionStatesAndProbs(state, action)
+                    for transition_state, probability in transition_states:
+                        if transition_state in predecessors:
+                            predecessors[transition_state].append(state)
+                        else:
+                            predecessors[transition_state] = [state]
+                    # Sets the Q Value
+                    actions_counter[action] = self.computeQValueFromValues(state, action)
+
+                # argMax returns the key with the biggest value in the counter
+                max_action = actions_counter.argMax()
+                diff = abs(self.values[state] - actions_counter[max_action]) # Max Q Value
+                # -diff because min queue and we want max
+                queue.update(state, -diff)
+
+        for i in range(0, self.iterations):
+            if not queue.isEmpty():
+                current = queue.pop()
+                if not self.mdp.isTerminal(current):
+                    actions_counter = util.Counter()
+                    actions = self.mdp.getPossibleActions(current)
+
+                    # Get max Q Value of action states
+                    for action in actions:
+                        # Set Q Value for action
+                        actions_counter[action] = self.computeQValueFromValues(current, action)
+
+                    # argMax returns the key with the biggest value in the counter
+                    max_action = actions_counter.argMax()
+                    # Update value
+                    self.values[current] = actions_counter[max_action]
+
+                    for predecessor in predecessors:
+                        if not self.mdp.isTerminal(predecessor):
+                            predecessor_actions_counter = util.Counter()
+                            predecessor_actions = self.mdp.getPossibleActions(predecessor)
+
+                            # Find max predecessor Q value
+                            for predecessor_action in predecessor_actions:
+                                # Set Q Value for action
+                                predecessor_actions_counter[predecessor_action] = self.computeQValueFromValues(predecessor, predecessor_action)
+                            predecessor_max_action = predecessor_actions_counter.argMax()
+
+                            diff = abs(self.values[predecessor] - predecessor_actions_counter[predecessor_max_action])
+                            # Add back on to queue is the difference is higher than our tolerance
+                            if diff > self.theta:
+                                queue.update(predecessor, -diff)
